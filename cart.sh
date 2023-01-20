97a68d4c1233230d9fb94954b7017c8dd14390f0
Yemi <tywonderkid@yahoo.com>
2023-01-19 18:45:32 -0600

    Cart Script

--- cart.sh
+++ cart.sh
@@ -3,13 +3,13 @@ yum install nodejs -y
 
 useradd roboshop
 
-$ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
-$ cd /home/roboshop
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
 rm -rf cart
-$ unzip -o /tmp/cart.zip
-$ mv cart-main cart
-$ cd cart
-$ npm install
+unzip -o /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
 
 #sed -i -e 's/REDIS_ENPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' systemd.service
 
