0ce2315471411731dce7d751bd17cd7c04102735
Yemi <tywonderkid@yahoo.com>
2023-01-19 18:43:42 -0600

    Cart Script

---
+++ cart.sh
@@ -0,0 +1,19 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+
+useradd roboshop
+
+$ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+$ cd /home/roboshop
+rm -rf cart
+$ unzip -o /tmp/cart.zip
+$ mv cart-main cart
+$ cd cart
+$ npm install
+
+#sed -i -e 's/REDIS_ENPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' systemd.service
+
+# mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+# systemctl daemon-reload
+# systemctl start cart
+# systemctl enable cart
