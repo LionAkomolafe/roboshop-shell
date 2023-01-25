583939b9cffa0fe59f138cc2874c9f0ca198d3f0
Yemi <tywonderkid@yahoo.com>
2023-01-25 10:26:17 -0600

    Cart Script

--- cart.sh
+++ cart.sh
@@ -1,50 +1,52 @@
+COMPONENT=cart
 source common.sh
 
 PRINT "Install NodeJS Repository"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG
 STAT $?
 
 PRINT "Install NodeJS"
-yum install nodejs -y
+yum install nodejs -y &>>$LOG
 STAT $?
 
-PRINT "Create User Application"
+PRINT "Create User Application" &>>$LOG
 useradd roboshop
 STAT $?
 
 PRINT "Download App Content"
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>$LOG
 STAT $?
 
 PRINT "Remove Previous Version of App"
-cd /home/roboshop
-rm -rf cart
+cd /home/roboshop &>>$LOG
+rm -rf cart &>>$LOG
 STAT $?
 
-unzip -o /tmp/cart.zip
-mv cart-main cart
+PRINT "Extracting App Content"
+unzip -o /tmp/cart.zip &>>$LOG
+mv cart-main cart &>>$LOG
 cd cart
 
 PRINT "Install NodeJS Dependencies"
-npm install
+npm install &>>LOG
 STAT $?
 
 PRINT "Reconfigure Endpoints for SystemD Configuration"
-sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/cart/systemd.service
+sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/cart/systemd.service &>>LOG
 STAT $?
 
 PRINT "Rename Configuration"
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>$LOG
 STAT $?
 
 PRINT "Reload SystemD"
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOG
 STAT $?
 
 PRINT "Restart Cart"
-systemctl restart cart
+systemctl restart cart &>>$LOG
 STAT $?
 
 PRINT "Enable Cart Service"
-systemctl enable cart
+systemctl enable cart &>>$LOG
 STAT $?
