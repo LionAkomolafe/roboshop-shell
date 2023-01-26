d2967f3fe33aaafd5d9c307a6131cc141cc9cebc
Yemi <tywonderkid@yahoo.com>
2023-01-26 13:08:37 -0600

    Common script

--- common.sh
+++ common.sh
@@ -36,27 +36,27 @@ NODEJS() {
   STAT $?
 
   PRINT "Download App Content"
-  curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>$LOG
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>$LOG
   STAT $?
 
   PRINT "Remove Previous Version of App"
   cd /home/roboshop &>>$LOG
-  rm -rf cart &>>$LOG
+  rm -rf ${COMPONENT} &>>$LOG
   STAT $?
 
   PRINT "Extracting App Content"
-  unzip -o /tmp/cart.zip &>>$LOG
+  unzip -o /tmp/${COMPONENT}.zip &>>$LOG
   STAT $?
 
-  mv cart-main cart
-  cd cart
+  mv ${COMPONENT}-main ${COMPONENT}
+  cd ${COMPONENT}
 
   PRINT "Install NodeJS Dependencies"
   npm install &>>LOG
   STAT $?
 
   PRINT "Reconfigure Endpoints for SystemD Configuration"
-  sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/cart/systemd.service &>>LOG
+  sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/${COMPONENT}/systemd.service &>>LOG
   STAT $?
 
   #PRINT "Rename Configuration"
@@ -67,11 +67,12 @@ NODEJS() {
   systemctl daemon-reload &>>$LOG
   STAT $?
 
-  PRINT "Restart Cart"
-  systemctl restart cart &>>$LOG
+  PRINT "Restart ${COMPONENT}"
+  systemctl restart ${COMPONENT} &>>$LOG
   STAT $?
 
-  PRINT "Enable Cart Service"
-  systemctl enable cart &>>$LOG
+  PRINT "Enable ${COMPONENT} Service"
+  systemctl enable ${COMPONENT} &>>$LOG
   STAT $?
+
 }
