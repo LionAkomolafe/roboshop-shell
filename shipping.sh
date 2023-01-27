45782bdd0a41bc3a8ef0c020229c3df0eaaae828
Yemi <tywonderkid@yahoo.com>
2023-01-26 21:01:56 -0600

    Shipping Script

---
+++ shipping.sh
@@ -0,0 +1,52 @@
+COMPONENT=shipping
+source common.sh
+
+PRINT "Install JavaScript"
+yum install maven -y
+STAT $?
+
+PRINT "Create Application User"
+useradd roboshop
+STAT $?
+
+
+cd /home/roboshop
+PRINT "Download App Content"
+curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+STAT $?
+
+unzip -o /tmp/shipping.zip
+PRINT "Remove Former Version of App"
+rm -rf shipping
+STAT $?
+
+mv shipping-main shipping
+cd shipping
+
+PRINT "Clean Package"
+mvn clean package
+STAT $?
+
+PRINT "Rename File"
+mv target/shipping-1.0.jar shipping.jar
+STAT $?
+
+PRINT "Reconfigure Cart and MySQL Endpoints"
+sed -i -e "s/CARTENDPOINT/cart.devopsb69.online/" -e "s/DBHOST/mysql.devopsb69.online/" systemd.service
+STAT $?
+
+PRINT "Rename Destination File"
+mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+STAT $?
+
+PRINT "Reload Daemon"
+systemctl daemon-reload
+STAT $?
+
+PRINT "Enable Shipping Service"
+systemctl enable shipping
+STAT $?
+
+PRINT "Start Shipping Service"
+systemctl start shipping
+STAT $?
