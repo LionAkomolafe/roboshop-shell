85c8ae15f70e9a2d1aac2ac877da2643a8e26a28
Yemi <tywonderkid@yahoo.com>
2023-02-06 15:02:36 -0600

    vars.yml script

--- common.sh
+++ common.sh
@@ -1,73 +1,62 @@
-PRINT() {
-  echo ------------ $1 ------------ >>$LOG
-  echo -e "\e[33m$1\e[0m"
-}
-
 STAT() {
   if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
-    echo View the error in $LOG file
-    exit
+    echo Check the error in $LOG file
+    exit 1
   fi
+}
+
+PRINT() {
+  echo " --------------------  $1 ------------------" >>${LOG}
+  echo -e "\e[33m$1\e[0m"
 }
 
 LOG=/tmp/$COMPONENT.log
 rm -f $LOG
+set-hostname -skip-apply $COMPONENT
 
 DOWNLOAD_APP_CODE() {
-  if [ ! -z "$APP_USER" ]; then
-      PRINT "Adding Application User"
-      id roboshop &>>$LOG
-      if [ $? -ne 0 ]; then
-        useradd roboshop &>>$LOG
-      fi
-      STAT $?
+  if [ ! -z  "$APP_USER" ]; then
+    PRINT "Adding Application User"
+    id roboshop &>>$LOG
+    if [ $? -ne 0 ]; then
+      useradd roboshop &>>$LOG
+    fi
+    STAT $?
   fi
 
   PRINT "Download App Content"
-    curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/roboshop-devops-project/$COMPONENT/archive/main.zip" &>>$LOG
-    STAT $?
+  curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/roboshop-devops-project/$COMPONENT/archive/main.zip" &>>$LOG
+  STAT $?
 
-    PRINT "Remove Previous Version of App"
-    cd $APP_LOC &>>$LOG
-    rm -rf $CONTENT &>>$LOG
-    STAT $?
+  PRINT "Remove Previous Version of App"
+  cd $APP_LOC &>>$LOG
+  rm -rf $CONTENT &>>$LOG
+  STAT $?
 
-    PRINT "Extracting App Content"
-    unzip -o /tmp/$COMPONENT.zip &>>$LOG
-    STAT $?
+  PRINT "Extracting App Content"
+  unzip -o /tmp/$COMPONENT.zip &>>$LOG
+  STAT $?
 }
 
 SYSTEMD_SETUP() {
-  PRINT "Reconfigure the Endpoints for SystemD Configuration"
-    sed -i -e 's/REDIS_ENDPOINT/redis.kingyamza.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.kingyamza.online/' /home/roboshop/${COMPONENT}/systemd.service &>>LOG
-    mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-    STAT $?
+  PRINT "Configure Endpoints for SystemD Configuration"
+  sed -i -e 's/MONGO_DNSNAME/dev-mongodb.kingyamza.online/' -e 's/REDIS_ENDPOINT/dev-redis.kingyamza.online/' -e 's/CATALOGUE_ENDPOINT/dev-catalogue.kingyamza.online/' -e 's/MONGO_ENDPOINT/dev-mongodb.kingyamza.online/' -e 's/CARTENDPOINT/dev-cart.kingyamza.online/' -e 's/DBHOST/dev-mysql.kingyamza.online/' -e 's/AMQPHOST/dev-rabbitmq.kingyamza.online/' -e 's/CARTHOST/dev-cart.kingyamza.online/' -e 's/USERHOST/dev-user.kingyamza.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  STAT $?
 
-    #PRINT "Rename Configuration"
-    #mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>$LOG
-    #STAT $?
-
-    PRINT "Reload SystemD"
-    systemctl daemon-reload &>>$LOG
-    STAT $?
-
-    PRINT "Restart ${COMPONENT}"
-    systemctl restart ${COMPONENT} &>>$LOG
-    STAT $?
-
-    PRINT "Enable ${COMPONENT} Service"
-    systemctl enable ${COMPONENT} &>>$LOG
-    STAT $?
+  PRINT "Restart ${COMPONENT}"
+  systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
+  STAT $?
 }
 
 NODEJS() {
   APP_LOC=/home/roboshop
   CONTENT=$COMPONENT
   APP_USER=roboshop
-  PRINT "Install NodeJS Repository"
+  PRINT "Install NodeJS Repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG
   STAT $?
 
@@ -75,38 +64,19 @@ NODEJS() {
   yum install nodejs -y &>>$LOG
   STAT $?
 
+
   DOWNLOAD_APP_CODE
 
   mv ${COMPONENT}-main ${COMPONENT}
   cd ${COMPONENT}
 
-  PRINT "Install NodeJS Dependencies"
-  npm install &>>LOG
+  PRINT "Install NodeJS Dependencies for App "
+  npm install &>>$LOG
   STAT $?
 
   SYSTEMD_SETUP
-
-  PRINT "Reconfigure Endpoints for SystemD Configuration"
-  sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/${COMPONENT}/systemd.service &>>LOG
-  STAT $?
-
-  #PRINT "Rename Configuration"
-  #mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>$LOG
-  #STAT $?
-
-  PRINT "Reload SystemD"
-  systemctl daemon-reload &>>$LOG
-  STAT $?
-
-  PRINT "Restart ${COMPONENT}"
-  systemctl restart ${COMPONENT} &>>$LOG
-  STAT $?
-
-  PRINT "Enable ${COMPONENT} Service"
-  systemctl enable ${COMPONENT} &>>$LOG
-  STAT $?
-
 }
+
 
 JAVA() {
   APP_LOC=/home/roboshop
@@ -114,32 +84,43 @@ JAVA() {
   APP_USER=roboshop
 
   PRINT "Install Maven"
-  yum install maven -y &>>$LOG
+  yum install maven -y  &>>$LOG
   STAT $?
 
   DOWNLOAD_APP_CODE
 
+  mv ${COMPONENT}-main ${COMPONENT}
+  cd ${COMPONENT}
+
   PRINT "Download Maven Dependencies"
-  mvn clean package &>>$LOG && mv target/$COMPONENT-1.0.jar $COMPONENT.jar &>>$LOG
+  mvn clean package &>>$LOG  && mv target/$COMPONENT-1.0.jar $COMPONENT.jar &>>$LOG
   STAT $?
 
-  PRINT "Reconfigure Cart and MySQL Endpoints"
-  sed -i -e "s/CARTENDPOINT/cart.devopsb69.online/" -e "s/DBHOST/mysql.devopsb69.online/" systemd.service &>>$LOG
+  SYSTEMD_SETUP
+
+}
+
+PYTHON() {
+  APP_LOC=/home/roboshop
+  CONTENT=$COMPONENT
+  APP_USER=roboshop
+
+  PRINT "Install Python"
+  yum install python36 gcc python3-devel -y &>>$LOG
   STAT $?
 
-  PRINT "Rename Destination File"
-  mv /home/roboshop/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service &>>$LOG
+  DOWNLOAD_APP_CODE
+  mv ${COMPONENT}-main ${COMPONENT}
+  cd ${COMPONENT}
+
+  PRINT "Install Python Dependencies"
+  pip3 install -r requirements.txt  &>>$LOG
   STAT $?
 
-  PRINT "Reload Daemon"
-  systemctl daemon-reload &>>$LOG
-  STAT $?
+  USER_ID=$(id -u roboshop)
+  GROUP_ID=$(id -g roboshop)
+  sed -i -e "/uid/ c uid = ${USER_ID}" -e "/uid/ c uid = ${GROUP_ID}" ${COMPONENT}.ini
 
-  PRINT "Enable $COMPONENT Service"
-  systemctl enable $COMPONENT &>>$LOG
-  STAT $?
+  SYSTEMD_SETUP
 
-  PRINT "Start $COMPONENT Service"
-  systemctl start $COMPONENT &>>$LOG
-  STAT $?
 }
