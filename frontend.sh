775bfaa35c1ed9ac8688c7e0c8f31187c2d24024
Yemi <tywonderkid@yahoo.com>
2023-02-06 15:06:46 -0600

    vars.yml script

--- frontend.sh
+++ frontend.sh
@@ -13,13 +13,17 @@ DOWNLOAD_APP_CODE
 mv frontend-main/static/* .
 
 PRINT "Copy RoboShop Configuration File"
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+PRINT "Update RoboShop Configuration"
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.kingyamza.online/'  -e '/user/ s/localhost/dev-user.kingyamza.online/' -e '/cart/ s/localhost/dev-cart.kingyamza.online/' -e '/shipping/ s/localhost/dev-shipping.kingyamza.online/' -e '/payment/ s/localhost/dev-payment.kingyamza.online/' /etc/nginx/default.d/roboshop.conf
 STAT $?
 
 PRINT "Enable Nginx Service"
 systemctl enable nginx &>>$LOG
 STAT $?
 
-PRINT "Restart Nginx Service"
+PRINT "Start Nginx Service"
 systemctl restart nginx &>>$LOG
 STAT $?
