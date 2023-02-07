519834acd5ebea24936f684a05d53df6eb37c226
Yemi <tywonderkid@yahoo.com>
2023-02-07 16:02:18 -0600

    Crete Roboshop Servers Script

--- create-roboshop-single-server.sh
+++ create-roboshop-single-server.sh
@@ -17,29 +17,4 @@ create_ec2() {
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 exit
-  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
-
-
-## Main Program
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-if [ -z "${AMI_ID}" ]; then
-  echo "AMI_ID not found"
-  exit 1
-fi
-
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
-if [ -z "${SGID}" ]; then
-  echo "Given Security Group does not exit"
-  exit 1
-fi
-
-if [ -z "$1" ]; then
-  echo Input Component Name is needed
-  exit 1
-fi
-
-component=$1
-COMPONENT="${env}-${component}"
-create_ec2
